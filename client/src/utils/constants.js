321cd288202abc62cee5af0ecd9d9694c1495387
ChipDragon <tantien2k44@gmail.com>
2025-12-23 03:56:33 +0700

    test deploy

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,5 @@
-// client/src/utils/constants.js
-
-export const NETWORK = 'devnet';
-export const PACKAGE_ID = "0xe7fd024777d5e1f814809f4c4961a793c0855bc17b9d241f4f8165287b7caae7";
-export const GAME_INFO_ID = "0xba7cb3af97d5b48e7bcc1e3b239f4f995a45a86b3cd6b05067e40bf471023ecb";
-export const CLOCK_ID = "0x6";
-export const STAMINA_COST = 10;
+export const NETWORK = import.meta.env.VITE_NETWORK;
+export const PACKAGE_ID = import.meta.env.VITE_PACKAGE_ID;
+export const GAME_INFO_ID = import.meta.env.VITE_GAME_INFO_ID;
+export const CLOCK_ID = import.meta.env.VITE_CLOCK_ID;
+export const STAMINA_COST = Number(import.meta.env.VITE_STAMINA_COST);
